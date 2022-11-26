b411efae7db71c6c0dd72055602c99423ffb94f7
Mateo <mateo.azv3@gmail.com>
2022-11-26 14:29:42 -0300

    Melhoria usando Joptionpane

--- src/main/java/programa/hello/Main.java
+++ src/main/java/programa/hello/Main.java
@@ -1,7 +1,8 @@
 package programa.hello;
 
+import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world  !!!");
+        JOptionPane.showMessageDialog(null,"Hello  World !!!");
     }
 }
